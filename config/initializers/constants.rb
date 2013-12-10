3b0b4509697e7883ed61060dd3aab0bc6175540c
Christopher Padua <x24padua22@yahoo.com>
2013-12-11 07:55:38 +0800

    First commit for login facebook in Ruby on Rails

---
+++ config/initializers/constants.rb
@@ -0,0 +1,3 @@
+APP_ID= '505664466188790' # please change!
+APP_SECRET= '990a3cad1cedcb4801208b00fb760634' # please change!
+SITE_URL = 'http://localhost:3000'
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+SampleKoalaRailsApp::Application.routes.draw do
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  root :to => 'home#index'
+
+  match 'home/callback' => 'home#callback'
+
+end
